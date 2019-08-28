af8f7a5380709a32c2c4778bfe97895901ba7ef8
Camily Kirlly Pereira Meireles <kirlly.p@academico.ifrn.edu.br>
2019-08-28 16:08:03 -0300

    Arquivo

--- Lista16/Ex01/MainWindow.xaml.cs
+++ Lista16/Ex01/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Calculo
+namespace Ex01
 {
     /// <summary>
     /// Interação lógica para MainWindow.xam
--- Lista16/Ex05/Numeros.cs
+++ Lista16/Ex05/Numeros.cs
@@ -8,5 +8,49 @@ namespace Ex05
 {
     class Numeros
     {
+        private int inicio, fim;
+        public void SetInicio(int i)
+        {
+            inicio = i;
+        }
+        public void SetFim(int f)
+        {
+            fim = f;
+        }
+        public int GetInicio()
+        {
+            return inicio;
+
+        }
+        public int GetFim()
+        {
+            return fim;
+
+        }
+        public int[] Calcular(bool pares, bool impares)
+        {
+            int[] intervalo = new int[fim - inicio + 1];
+            int c = 0;
+            for (int x = inicio; x <= fim; x++)
+            {
+                if (x % 2 == 0 && pares == true && impares == false)
+                {
+                    intervalo[c] = x;
+                    ++c;
+                }
+                if (x != 2 == 0 && impares == true && pares == false)
+                {
+                    intervalo[c] = x;
+                    ++c;
+                }
+
+                if (pares == true && impares == true)
+                {
+                    intervalo[c] = x;
+                    ++c;
+                }
+                
+            }
+        }
     }
 }
