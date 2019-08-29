89d38b2d715fc3e76833ec50a41b2a95bded3345
Camily Kirlly Pereira Meireles <kirlly.p@academico.ifrn.edu.br>
2019-08-29 11:54:49 -0300

    arquivo

---
+++ Lista16/Ex06/Bingo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06
+{
+    class Bingo
+    {
+        private int NumBolas;
+        public void SetNumBolas(int n)
+        {
+            NumBolas = n;
+        }
+        
+    }
+}
