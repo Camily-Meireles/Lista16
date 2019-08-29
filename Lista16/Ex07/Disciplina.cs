0a849bd78d81e63ba6a1ef2c010a0d14d389a849
Camily Kirlly Pereira Meireles <kirlly.p@academico.ifrn.edu.br>
2019-08-29 15:43:46 -0300

    arquivo

---
+++ Lista16/Ex07/Disciplina.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    class Disciplina
+    {
+        private string nome;
+        private string semestre;
+        private int media;
+        private bool aprovado;
+        public void SetNome(string n)
+        {
+            nome = n;
+        }
+        public void SetSemestre(string s)
+        {
+            semestre = s;
+        }
+        public void  SetMedia(int m)
+        {
+            media = m;
+        }
+        public void SetAprovado(bool a)
+        {
+            aprovado = a;
+        }
+
+
+        public string GetNome()
+        {
+            return nome;
+        }
+        public string GetSemestre()
+        {
+            return semestre;
+        }
+        public int GetMedia()
+        {
+            return media;
+        }
+        public bool Getaprovado()
+        {
+            return aprovado;
+        }
+
+
+
+
+
+    }
+}
---
+++ Lista16/Ex07/Historico.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex07
+{
+    class Historico
+    {
+        private string aluno;
+        int c = 0;
+        private Disciplina[] disciplina = new Disciplina[10];
+        public void SetAluno(string a)
+        {
+            aluno = a;
+        }
+        public void Inserir(Disciplina d)
+        {
+            disciplina[c++] = d;
+        }
+        public Disciplina[] Listar()
+        {
+            Disciplina[] di = new Disciplina[c];
+            Array.Copy(disciplina, di, c);
+            return di;
+        }
+        public double ira()
+        {
+            Disciplina i = new Disciplina();
+            foreach (Disciplina d in i.GetMedia()) ;
+            re
+
+
+        } 
+
+
+    }
+}
